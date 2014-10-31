28faa3b1860e6cf5e1b21f2272ad7a24e448ff84
Gabriel Serralha Miró <gsmiro@gmail.com>
2014-10-31 17:38:45 -0200

    Create store in first run. Missing web.xml config to use it.

--- pg-tomcat/dockerbuild/start.sh
+++ pg-tomcat/dockerbuild/start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+if [ -f $CATALINA_HOME/keystore ]
+then
+	echo "Keystore already created"
+else
+	openssl ca -config "$OPENSSL_CONF" -extensions v3_req -md sha1 -infiles server.csr
+
+	keytool -import -alias ca -file $CA_HOME/ca/cacert.pem -keystore $CATALINA_HOME/.keystore -storepass $(cat $DOCKERBUILD/tomcat/storepasswd)
+	keytool -import -alias server -file $CA_HOME/certs/$(cat $CA_HOME/serial).pem -keystore $CATALINA_HOME/.keystore -storepass $(cat $DOCKERBUILD/tomcat/storepass)
+	
+fi
+
 sudo -u postgres $PGBIN/bin/pg_ctl $@ -D $PGCLUSTER --log /var/log/metaway/postgresql.log
 sudo -u $TOMCAT_USER $CATALINA_HOME/bin/catalina.sh $@
 #INFINITE LOOP TO BLOCK CONTAINER
